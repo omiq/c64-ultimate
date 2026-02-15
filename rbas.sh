d098e7241521d9f7527edc9f598e8465d2e9ef53
omiq <chris@chrisg.com>
2026-02-15 20:10:14 +0000

    html working

--- rbas.sh
+++ rbas.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-petcat -v -2 -o prg.prg -- /home/chrisg/github/c64-ultimate/word-search.bas
+petcat -v -f -ic -2 -o prg.prg -- /home/chrisg/github/c64-ultimate/word-search.bas
 python3 runner.py prg.prg
 
