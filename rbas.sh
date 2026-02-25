7d1f8eed12f750f1613bb3584719f53745369a64
omiq <chris@chrisg.com>
2026-02-25 14:05:43 +0000

    update the launch script

--- rbas.sh
+++ rbas.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-petcat -v -f -ic -2 -o prg.prg -- /home/chrisg/github/c64-ultimate/word-search.bas
+# Lowercase the BASIC source for petcat (it expects lowercase keywords)
+tr 'A-Z' 'a-z' < /home/chrisg/github/c64-ultimate/word-search.bas | \
+  petcat -v -f -w2 -c -o prg.prg --
 python3 runner.py prg.prg
 
