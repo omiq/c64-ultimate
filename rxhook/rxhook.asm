19c79b95ecc5ac0f14c9100b76c00a357f28e295
Chris Garrett <chris@chrisg.com>
2026-06-07 16:39:33 +0100

    back to a non-corrupted http-get.bas

---
+++ rxhook/rxhook.asm
@@ -0,0 +1,37 @@
+; Minimal NMI ring-buffer ISR for direct-ACIA BASIC programs.
+; Ring page $C200, tail $A8 (ISR), head $A7 (BASIC). Load at $0C00.
+
+RING      = $C200
+ZP_HEAD   = $A7
+ZP_TAIL   = $A8
+ACIA_DATA = $DE00
+ACIA_STAT = $DE01
+ACIA_CMD  = $DE02
+
+.segment "CODE"
+.org $0C00
+
+nmi_handler:
+        pha
+        txa
+        pha
+        tya
+        pha
+        lda ACIA_STAT
+        ldx #$0B
+        stx ACIA_CMD
+        and #$08
+        beq done
+        lda ACIA_DATA
+        ldy ZP_TAIL
+        sta RING,y
+        inc ZP_TAIL
+        lda #$09
+        sta ACIA_CMD
+done:
+        pla
+        tay
+        pla
+        tax
+        pla
+        rti
